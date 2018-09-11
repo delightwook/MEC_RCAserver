b2a34fe235475167a50da0ac8c31f1193ebbabbc
delightwook91 <delightwook@naver.com>
2018-09-11 13:18:10 +0900

    Completed communication between server and agent

--- rca_server.py
+++ rca_server.py
@@ -9,6 +9,7 @@ api = Api(app)
 """
 Test Code
 url = 'curl -i -H \"Content-Type: application/json\" -X POST -d ' + '\'' + total_request + '\'' + ' http://127.0.0.1:5050/Test'
+curl -i -H "Content-Type: application/json" -X POST -d '{"Hello":"hi"}' http://127.0.0.1:7070/MECrcaserver
 os.system(url)
 """
 @app.route('/MECrcaserver',methods=['POST','PUT'])
@@ -22,4 +23,4 @@ def rca_server():
     return ''
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port = 7070,debug = True)
+    app.run(host='192.168.11.11',port = 7071,debug = True)
