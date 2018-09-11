99cf9deb8730abe7f25f5d91e3f968d6cf7e7f49
delightwook91 <delightwook@naver.com>
2018-09-11 11:46:57 +0900

    Modified rca_server

--- rca_server.py
+++ rca_server.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse, abort
 
-import json
-import ast
 app = Flask(__name__)
 api = Api(app)
 
@@ -13,8 +11,6 @@ Test Code
 url = 'curl -i -H \"Content-Type: application/json\" -X POST -d ' + '\'' + total_request + '\'' + ' http://127.0.0.1:5050/Test'
 os.system(url)
 """
-
-
 @app.route('/MECrcaserver',methods=['POST','PUT'])
 def rca_server():
     if not request.json:
